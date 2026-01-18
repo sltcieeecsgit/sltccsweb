0dfe4f61c2bf161fa919d0c222b96a1418cffe1d
Binara <binarayasas2003@gmail.com>
2026-01-18 23:07:57 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/csweb',
+  base: '/sltccsweb',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
